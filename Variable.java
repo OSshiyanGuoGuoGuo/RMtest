b02de5576d4d2bdbcb2259ebf5ea60ae04d2999d
WZJun <128673164+OSshiyanGuoGuoGuo@users.noreply.github.com>
2024-08-19 10:02:21 +0800

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,11 @@
+public class Variable {
+    public void extract() {
+        System.out.println("Price: " + 100 * 0.8);
+    }
+    public void calculate() {
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
